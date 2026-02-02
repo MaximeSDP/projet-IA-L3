41879aa44002dd3458a18835f89a782031fbca22
Lucas.A <lucas-arguimbau@LucasARG.>
2026-02-02 11:59:57 +0100

    dataset

--- data.py
+++ data.py
@@ -1,28 +1,23 @@
 from PIL import Image
-import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn import datasets
+from buildSampleFromPath  import buildSampleFromPath
 
-# Charger l'image
-image = Image.open("peacock.jpg")
+def createDataset(listImage):
+   
+    X = []
+    y = []
+    for image in listImage :
+        X.append(image.X_histo)
+        y.append(image.y_predicted_class)
 
-# Obtenir l'histogramme des couleurs
-histogram = image.histogram()
+    df = (pd.DataFrame(X),pd.DataFrame(y))
 
-# Extraire les comptes pour chaque canal
-l1 = histogram[0:256]  # Rouge
-l2 = histogram[256:512]  # Vert
-l3 = histogram[512:768]  # Bleu
+    return df
 
-# Tracer les histogrammes
-plt.figure(0)
-for i in range(0, 256):
-    plt.bar(i, l1[i], color=f'#{i:02x}0000', edgecolor=f'#{i:02x}0000', alpha=0.3)
 
-plt.figure(1)
-for i in range(0, 256):
-    plt.bar(i, l2[i], color=f'#{00:02x}{i:02x}00', edgecolor=f'#{00:02x}{i:02x}00', alpha=0.3)
-
-plt.figure(2)
-for i in range(0, 256):
-    plt.bar(i, l3[i], color=f'#{00:02x}00{i:02x}', edgecolor=f'#{00:02x}00{i:02x}', alpha=0.3)
-
-plt.show()   
+S = buildSampleFromPath('Init/Mer', 'Init/Ailleurs')
+print(S)
+data = createDataset(buildSampleFromPath('Init/Mer', 'Init/Ailleurs'))
+print(data)
