a874a1a4ff76eed766852add7544834cc41281db
m20003692 <lucas.maurin.2@etu.univ-amu.fr>
2026-02-02 10:52:06 +0100

    resize image integration

--- buildSampleFromPath.py
+++ buildSampleFromPath.py
@@ -1,6 +1,9 @@
 from os import listdir
 from os.path import isfile, join
 
+import resizeImage
+
+
 class ImageData:
 
     def __init__(self, name_path, y_true_class):
@@ -16,12 +19,20 @@ def buildSampleFromPath(path1, path2):
     other_path = [f for f in listdir(path2) if isfile(join(path2, f))]
     S = []
 
-    for i in sea_path:
-        S.append(ImageData(i, 1))
-    for i in other_path:
-        S.append(ImageData(i, -1))
+    for path in sea_path:
+        image = ImageData(path1+"/"+path, 1)
+        S.append(image)
+
+    for path in other_path:
+        image = ImageData(path2+"/"+path, -1)
+        S.append(image)
+
+    for s in S:
+        s.resized_image = resizeImage.resizeImage(s.name_path, 1080, 1920)
 
     return S
 
 
 S = buildSampleFromPath('Init/Mer', 'Init/Ailleurs')
+for image in S:
+    print(image.resized_image)
